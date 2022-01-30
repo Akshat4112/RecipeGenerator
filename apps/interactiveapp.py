98b346d4c190edd7a034249d2f862506f29f198d
Akshat <akshat41121995@gmail.com>
2022-01-30 18:40:13 +0100

    API Integrated

--- apps/interactiveapp.py
+++ apps/interactiveapp.py
@@ -1,19 +1,27 @@
 import streamlit as st
 import os
 import time
+import requests
+import json
 
 def app():
     st.text("Text Technology Project, Class of 21-22")
     st.text("Submitted to: Kerstin Jung")
     st.text("Submitted by: Silvia Cunico, Akshat Gupta")
-    text = st.text_input("Enter Ingredients")
-
+    text_inp = st.text_input("Enter Ingredients")
+    
     if(st.button("Generate")):
+        #API Call on text
+        url = "http://3.20.227.146:5001/predict_text"
+        payload = json.dumps({"text": text_inp})
+        headers = {'Content-Type': 'application/json'}
+        response = requests.request("POST", url, headers=headers, data=payload)
+        print(response.text)
+
         progress = st.progress(0)
         for i in range(100):
             time.sleep(0.1)
             progress.progress(i+1)        
-        #API Call on text
-        st.warning("Here is your Recipe! ")
-        text = "zwiebeln mit der Sauce anbraten, mit der Gurkenhälften und einem fein gehackten Käse bestreuen und etwa 6-8 Stunden schmoren lassen.Nach Ende dieser Zeit die Pilze aus dem Glas vierteln und mit dem restlichen"
-        st.success(text)
+        
+        st.warning("Da ist deine Rezepte!")
+        st.success(str(response.text))
